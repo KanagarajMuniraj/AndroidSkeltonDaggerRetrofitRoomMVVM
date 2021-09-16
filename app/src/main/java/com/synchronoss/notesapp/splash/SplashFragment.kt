ae6c1ef8754d4673607516809598db21624979c9
Kanagaraj M <kanaguraj82@gmail.com>
2021-09-16 17:22:57 +0530

    Refactored Splash

--- app/src/main/java/com/synchronoss/notesapp/splash/SplashFragment.kt
+++ app/src/main/java/com/synchronoss/notesapp/splash/SplashFragment.kt
@@ -1,4 +1,4 @@
-package com.synchronoss.notesapp
+package com.synchronoss.notesapp.splash
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,12 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.synchronoss.notesapp.R
 import com.synchronoss.notesapp.databinding.FragmentSecondBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class SplashFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
 
